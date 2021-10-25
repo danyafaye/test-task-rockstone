ab1e079ca403fd64a0ee52d072f55838a7718212
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-10-25 22:39:58 +0300

    small fixes with routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,13 @@
 import './Header.css';
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-/*import { Link } from 'react-router-dom';*/
-
-/*onClick={<Link to="/vars"/>}*/
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="App-header">
-            <NavLink to="/vars" className="app-nominalo">Варианты набора купюр</NavLink>
-            <NavLink to="/note" className="app-note">Вызов справки</NavLink>
+            <NavLink to="/vars" className="app-nominalo">ВАРИАНТЫ НАБОРА КУПЮР</NavLink>
+            <NavLink to="/note" className="app-note">ВЫЗОВ СПРАВКИ</NavLink>
+            <NavLink to="/app-input" className="app-input-navi">ГЛАВНАЯ СТРАНИЦА</NavLink>
         </header>
     )
 }
