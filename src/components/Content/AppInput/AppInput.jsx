0543493b415022e1c95fe98941d0df4476df10eb
Danila Serpokrylov <serpokrylov.danila@mail.ru>
2021-10-28 02:05:48 +0300

    added numpad

--- src/components/Content/AppInput/AppInput.jsx
+++ src/components/Content/AppInput/AppInput.jsx
@@ -1,43 +1,79 @@
 import './AppInput.css';
 import React, { useState } from 'react';
-import { NavLink, Route } from 'react-router-dom';
-
-/*document.querySelector("input").addEventListener('keypress',
-    function () {
-        this.value = this.value.replace(/[^\d]/g, '');
-    })*/
+/*<button>&#129044;</button>*/
 
 const AppInput = () => {
     const [val, setVal] = useState("");
     const [valOut, setValOut] = useState(val);
-    const [inputText, setInputText] = useState('');
+    const [inputVal, setInputVal] = useState('');
     const handlerChange = (e) => {
-        let text = e.target.value.replace(/[^0-9\,]/g, '');
-        setInputText(text)
-         setVal(text)
+        let replacedValue = e.target.value.replace(/[^0-9\,]/g, '');
+        setInputVal(replacedValue)
+        setVal(replacedValue)
     }
     const onKeyDown = (e) => {
-        if(e.key === "Enter" || e.key === "NumpadEnter"){
+        if (e.key === "Enter" || e.key === "NumpadEnter") {
             e.preventDefault()
             e.stopPropagation()
             setValOut(val)
         }
     }
+    const buttonClick = (e) => {
+        let bttnVal = e.target.value
+        setInputVal(inputVal + bttnVal)
+        setVal(inputVal + bttnVal)
+    }
     return (<div className="app-input-output-form">
-            <input type="text" className="app-input" value={inputText} onChange={handlerChange} onKeyDown={onKeyDown}/>
-            <button className="app-input-to-output" onClick={()=>setValOut(val)}>ВЫДАЧА</button>
-            <AppOutput valOut={valOut} />
+            <input type="text" className="app-input" value={inputVal} onChange={handlerChange} onKeyDown={onKeyDown}/>
+            <button className="app-input-to-output" onClick={() => setValOut(val)}>ВЫДАЧА</button>
+            <div className="app-numpad">
+                <div className="app-numpad-first-block">
+                    <button value={1} onClick={buttonClick}>1</button>
+                    <button value={2} onClick={buttonClick}>2</button>
+                    <button value={3} onClick={buttonClick}>3</button>
+                </div>
+                <div className="app-numpad-second-block">
+                    <button value={4} onClick={buttonClick}>4</button>
+                    <button value={5} onClick={buttonClick}>5</button>
+                    <button value={6} onClick={buttonClick}>6</button>
+                </div>
+                <div className="app-numpad-third-block">
+                    <button value={7} onClick={buttonClick}>7</button>
+                    <button value={8} onClick={buttonClick}>8</button>
+                    <button value={9} onClick={buttonClick}>9</button>
+                </div>
+                <div className="app-numpad-last-block">
+                    <button value={0} onClick={buttonClick}>0</button>
+                    <button value={","} onClick={buttonClick}>,</button>
+                </div>
+            </div>
+            <AppOutput valOut={valOut}/>
         </div>
     )
 }
 
 const AppOutput = (props) => {
-    if(props.valOut==="")return null;
-    return(
-    <div className="app-output-form">
-        <p className="output-sum">Введенная сумма:{props.valOut}</p>
-    </div>
-    )
+    if (props.valOut === "") return null;
+    let outputValue = props.valOut
+    String.prototype.replaceAt = function (index, replacement) {
+        return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+    }
+    if (outputValue.includes(",")) {
+        let index = outputValue.indexOf(",")
+        let newValue = outputValue.slice(index + 1)
+        return (
+            <div className="app-output-form">
+                <p className="output-sum">Введенная
+                    сумма: {Math.trunc(outputValue.replaceAt(index, "."))} руб., {newValue} коп. </p>
+            </div>
+        )
+    } else {
+        return (
+            <div className="app-output-form">
+                <p className="output-sum">Введенная сумма: {outputValue} руб.</p>
+            </div>
+        )
+    }
 }
 
 export default AppInput;
